108e2b9e6688c8eb1940e8e5807fbf6503761154
Mohan Vaze <vazem@amazon.com>
2019-03-07 14:29:38 -0600

    aws secrets manager

---
+++ src/com/adiva/utility/SecretsManager.java
@@ -0,0 +1,79 @@
+package com.adiva.utility;
+
+import java.util.Base64;
+
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.DecryptionFailureException;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import com.amazonaws.services.secretsmanager.model.InternalServiceErrorException;
+import com.amazonaws.services.secretsmanager.model.InvalidParameterException;
+import com.amazonaws.services.secretsmanager.model.InvalidRequestException;
+import com.amazonaws.services.secretsmanager.model.ResourceNotFoundException;
+
+public class SecretsManager {
+
+	public static void main (String args[]) {
+		System.out.println(getSecret());
+	}
+
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/java-dg-samples.html#prerequisites
+
+public static String getSecret() {
+
+    String secretName = "testsecret";
+    String region = "us-east-1";
+    String decodedBinarySecret = null;
+    String secret = null;
+    // Create a Secrets Manager client
+    AWSSecretsManager client  = AWSSecretsManagerClientBuilder.standard()
+                                    .withRegion(region)
+                                    .build();
+    
+    // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    // We rethrow the exception by default.
+    
+   
+    GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
+                    .withSecretId(secretName);
+    GetSecretValueResult getSecretValueResult = null;
+
+    try {
+        getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+    } catch (DecryptionFailureException e) {
+        // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw e;
+    } catch (InternalServiceErrorException e) {
+        // An error occurred on the server side.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw e;
+    } catch (InvalidParameterException e) {
+        // You provided an invalid value for a parameter.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw e;
+    } catch (InvalidRequestException e) {
+        // You provided a parameter value that is not valid for the current state of the resource.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw e;
+    } catch (ResourceNotFoundException e) {
+        // We can't find the resource that you asked for.
+        // Deal with the exception here, and/or rethrow at your discretion.
+        throw e;
+    }
+
+    // Decrypts secret using the associated KMS CMK.
+    // Depending on whether the secret is a string or binary, one of these fields will be populated.
+    if (getSecretValueResult.getSecretString() != null) {
+    	return secret = getSecretValueResult.getSecretString();
+    }
+    else {
+        return decodedBinarySecret = new String(Base64.getDecoder().decode(getSecretValueResult.getSecretBinary()).array());
+    }
+
+}
+}
